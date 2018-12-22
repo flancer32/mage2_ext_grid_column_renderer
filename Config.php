4aed0dd5c734dc7e4df6368697cff24da0073ccb
Alex Gusev <alex@flancer64.com>
2018-12-22 20:10:13 +0200

    Init

---
+++ Config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Container for module's constants (hardcoded configuration).
+ *
+ * Authors: Alex Gusev <alex@flancer64.com>
+ * Since: 2018
+ */
+
+namespace Flancer32\GridLink;
+
+class Config
+{
+    /**
+     * This module's name.
+     */
+    const MODULE = self::MODULE_VENDOR . '_' . self::MODULE_PACKAGE;
+    const MODULE_PACKAGE = 'GridLink';
+    const MODULE_VENDOR = 'Flancer32';
+
+}
---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Script to register M2-module
+ *
+ * Authors: Alex Gusev <alex@flancer64.com>
+ * Since: 2018
+ */
+
+use Flancer32\LoginAs\Config as Cfg;
+use Magento\Framework\Component\ComponentRegistrar as Registrar;
+
+Registrar::register(
+    Registrar::MODULE,
+    Cfg::MODULE,
+    __DIR__
+);
